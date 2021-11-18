ec774832fe8b3c0e53ef6b4f30a8ebeaa771802d
amanprasad <aman.prasad@1stdibs.com>
2021-11-18 12:06:28 +0530

    complex recursive pattern

--- src/main/java/com/cpp/randomQuestions/RecursivePatternPrint.java
+++ src/main/java/com/cpp/randomQuestions/RecursivePatternPrint.java
@@ -2,7 +2,8 @@ package com.cpp.randomQuestions;
 
 public class RecursivePatternPrint {
     public static void main(String[] args) {
-        printPattern(8);
+        // printPattern(8);
+        printComplexPattern(8);
     }
 
     private static void printPattern(int n){
@@ -17,5 +18,23 @@ public class RecursivePatternPrint {
         //System.out.println(n);
 
 
+    }
+
+    private static void printComplexPattern(int n){
+        if(n == 1){
+            System.out.println("1");
+            return;
+        }
+        for(int i = 1; i<=n; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        printComplexPattern(n-1);
+
+        for(int i = 1; i<=n; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
